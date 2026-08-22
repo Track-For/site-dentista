0c68281832bdef602333be58535f6683c92b37a3
Luiz Fhellippe <fhellippe04092001@gmail.com>
2026-08-22 01:22:30 -0300

    Add Cloudflare worker build output

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sites } from '@openai/sites-vite-plugin'
+import { cloudflare } from '@cloudflare/vite-plugin'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [vue(), sites()],
+  plugins: [vue(), sites(), cloudflare()],
   server: {
     host: '127.0.0.1',
   },
