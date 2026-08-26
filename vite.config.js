13bbc39084dfc1a96fed9ed83b04fb300c02310a
Luiz Fhellippe <fhellippe04092001@gmail.com>
2026-08-25 23:17:11 -0300

    Add production worker output

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { sites } from '@openai/sites-vite-plugin'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
-export default defineConfig({
-  plugins: [vue(), sites()],
+export default defineConfig(({ command }) => ({
+  plugins: [vue(), sites(), ...(command === 'build' ? [cloudflare()] : [])],
   server: {
     host: '0.0.0.0',
   },
-})
+}))
